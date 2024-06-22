9bfe272e04076b4326c03a24ee415d9f28dccdb6
khamaruayan <169046571+khamaruayan@users.noreply.github.com>
2024-06-22 15:23:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const express=require ('express')
-const homeRoute=require ('./routs/home')
-const app=express()
-const port=5000
+import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import dotenv from "dotenv"
+import route from "./routes/userRoutes.js"
 
-app.set('view engine', 'ejs')
-app.use('/home',homeRoute)
 
-app.listen(port, ()=>{
-    console.log('Example app listening on port ${port}!')
+const app=express();
+
+app.use(bodyParser.json());
+dotenv.config();
+const PORT=process.env.PORT || 5000;
+const MONGOURL=process.env.MONGO_URL;
+
+
+mongoose.connect(MONGOURL).then(()=>{
+    console.log("Database connected successful.")
+    app.listen(PORT, ()=>{
+        console.log('Server is running on port ${PORT}');
+    })
+}).catch((error)=>{
+    console.log(error)
 })
+
+app.use("/api/user", route);
